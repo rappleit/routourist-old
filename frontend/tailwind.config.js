b8185a4e6d3c4b7e84376007e4143d98f7a626fc
Asyraf Omar <asyrafomar68@gmail.com>
2023-03-21 15:42:14 +0800

    initialised next project and config tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend:{
+      fontFamily:{
+        titleFont:['Epilogue'],
+        bodyfont:['Inter']
+
+      }
+    },
+colors: {
+    'eggshell':'#F7F4E3',
+    'green':'#7CC290',
+    'gray':'#40413F',
   },
+fontWeight: {
+  'black':'900',
+  'medium':'500',
+
+},
+  },
+
   plugins: [],
 }
